9b29a2b224dfaa7317dbb937d5dfd5a052c42860
Federico Midaglia <fmidaglia@gocloud.la>
2026-06-01 23:55:29 -0300

    fix(terraform): update required_version

--- examples/complete/providers.tf
+++ examples/complete/providers.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.10"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 6.0"
-    }
-  }
-}
-
 provider "aws" {
   region = local.metadata.aws_region
 }
